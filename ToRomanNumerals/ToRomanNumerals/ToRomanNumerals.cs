72a708c2364f1c9f482b092ce0734f1117e31d73
Marco Röhrs <marco.roehrs@bgt-germany.de>
2016-03-04 13:43:49 +0100

    ToRomanNumerals Kata vorbereitet

---
+++ ToRomanNumerals/ToRomanNumerals/ToRomanNumerals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToRomanNumerals
+{
+    public class ToRomanNumerals
+    {
+        public string Generate(int decimalNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToRomanNumerals/ToRomanNumerals/ToRomanNumeralsTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToRomanNumerals
+{
+    [TestClass]
+    public class ToRomanNumeralsTest    
+    {
+        private ToRomanNumerals _toRomanNumerals;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            _toRomanNumerals = new ToRomanNumerals();
+        }
+
+        [TestMethod]
+        public void Decimal_1_Results_In_I()
+        {
+            Assert.AreEqual("I", _toRomanNumerals.Generate(1));
+        }
+    }
+
+}
