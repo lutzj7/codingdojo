885cca37d06e2885663e856709a46fdb92fabeb0
Marco Röhrs <marco.roehrs@bgt-germany.de>
2016-03-07 12:08:40 +0100

    ToRomanNumerals - Ergebnis aus Coding-Dojo vom 04.03.2016

--- ToRomanNumerals/ToRomanNumerals/ToRomanNumerals.cs
+++ ToRomanNumerals/ToRomanNumerals/ToRomanNumerals.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ToRomanNumerals
 {
@@ -6,7 +8,94 @@ namespace ToRomanNumerals
     {
         public string Generate(int decimalNumber)
         {
-            throw new NotImplementedException();
+            string result = "";
+            SortedDictionary<int, string> romanDic = new SortedDictionary<int, string>()
+            {
+                {1000, "M"},
+                {900, "CM"},
+                {500, "D"},
+                {400, "CD"},
+                {100, "C"},
+                {90, "XC"},
+                {50, "L"},
+                {40, "XL"},
+                {10, "X"},
+                {9, "IX"},
+                {5, "V"},
+                {4, "IV"},
+                {1, "I"}
+            };
+
+            int current = decimalNumber;
+
+            while (current > 0)
+            {
+                foreach (var item in romanDic.Reverse())
+                {
+                    if (current >= item.Key)
+                    {
+                        current -= item.Key;
+                        result += item.Value;
+                        break;
+                    }
+                }
+            }
+            //int tmpDecimalNumber = decimalNumber;
+            //while (tmpDecimalNumber > 0)
+            //{
+            //    for (int i = romanDic.Values.Count - 1; i >= 0; i--)
+            //    {
+            //        if (romanDic.Keys.ToList()[i] == Math.Abs(tmpDecimalNumber))
+            //        {
+            //            result += romanDic.Values.ToList()[i];
+            //            tmpDecimalNumber -= romanDic.Keys.ToList()[i];
+            //        }
+            //        else if (romanDic.Keys.ToList()[i] < Math.Abs(tmpDecimalNumber))
+            //        {
+            //            if (romanDic.Keys.Count < i)
+            //            {
+            //                result += romanDic.Values.ToList()[i + 1];
+            //                tmpDecimalNumber -= romanDic.Keys.ToList()[i];
+            //            }
+            //        }
+
+            //    }
+            //}
+            
+
+            //while (decimalNumber > 0)
+            //{
+            //    if (decimalNumber > 8 && decimalNumber <= 10)
+            //    {
+            //        result += "X";
+            //        int dn = decimalNumber;
+            //        decimalNumber -= 10;
+
+            //        if (10 - dn > 0)
+            //        {
+            //            result = 'I' + result;
+            //            decimalNumber += 1;
+            //        }
+
+            //    }
+            //    else if (decimalNumber == 5)
+            //    {
+            //        result = "V";
+            //        decimalNumber -= 5;
+            //    }
+            //    else if (decimalNumber == 4)
+            //    {
+            //        result = "IV";
+            //        decimalNumber -= 4;
+            //    }
+            //    else if (decimalNumber < 4)
+            //    {
+            //        result = result + "I" ;
+            //        decimalNumber -= 1;
+            //    }
+            //}
+
+            return result;
         }
     }
 }
--- ToRomanNumerals/ToRomanNumerals/ToRomanNumeralsTest.cs
+++ ToRomanNumerals/ToRomanNumerals/ToRomanNumeralsTest.cs
@@ -22,6 +22,67 @@ namespace ToRomanNumerals
         {
             Assert.AreEqual("I", _toRomanNumerals.Generate(1));
         }
+
+        [TestMethod]
+        public void Decimal_2_Results_In_II()
+        {
+            Assert.AreEqual("II", _toRomanNumerals.Generate(2));
+        }
+
+        [TestMethod]
+        public void Decimal_4_Results_In_IV()
+        {
+            Assert.AreEqual("IV", _toRomanNumerals.Generate(4));
+        }
+
+        [TestMethod]
+        public void Decimal_5_Results_In_V()
+        {
+            Assert.AreEqual("V", _toRomanNumerals.Generate(5));
+        }
+
+
+        [TestMethod]
+        public void Decimal_9_Results_In_IX()
+        {
+            Assert.AreEqual("IX", _toRomanNumerals.Generate(9));
+        }
+
+        [TestMethod]
+        public void Decimal_10_Results_In_X()
+        {
+            Assert.AreEqual("X", _toRomanNumerals.Generate(10));
+        }
+
+        [TestMethod]
+        public void Decimal_42_Results_In_XLII()
+        {
+            Assert.AreEqual("XLII", _toRomanNumerals.Generate(42));
+        }
+
+        [TestMethod]
+        public void Decimal_99_Results_In_XCIX()
+        {
+            Assert.AreEqual("XCIX", _toRomanNumerals.Generate(99));
+        }
+
+        [TestMethod]
+        public void Decimal_2013_Results_In_MMXIII()
+        {
+            Assert.AreEqual("MMXIII", _toRomanNumerals.Generate(2013));
+        }
+
+        [TestMethod]
+        public void Decimal_999_Results_In_CMXCIX()
+        {
+            Assert.AreEqual("CMXCIX", _toRomanNumerals.Generate(999));
+        }
+
+        [TestMethod]
+        public void Decimal_1789_Results_In_MDCCLXXXIX()
+        {
+            Assert.AreEqual("MDCCLXXXIX", _toRomanNumerals.Generate(1789));
+        }
     }
 
 }
